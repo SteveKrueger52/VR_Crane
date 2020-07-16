b13a7b20e78f2b63b72b95630d1b80d810e2a7d8
Xinyun <xincao.xc@gmail.com>
2020-07-16 03:54:42 +0100

    Working 'magnet'

--- Assets/_CraneGame/Scripts/Magnet.cs
+++ Assets/_CraneGame/Scripts/Magnet.cs
@@ -7,23 +7,35 @@ using UnityEngine.UI;
 
 public class Magnet : MonoBehaviour
 {
-    public Boolean buttonPress=false; //unsure how you plan to have this value change, feel free to add a simple function to
+    public Boolean buttonPress=true; //unsure how you plan to have this value change, feel free to add a simple function to
                                 //change the value of this boolean when the button is pressed, or make this an external reference
-
+    public GameObject hook;
     public void OnTriggerEnter(Collider other)
     {
         if (buttonPress == true)
         {
-            Debug.Log("true");
-            // attachedObject = other.gameObject;
-            other.gameObject.GetComponent<Rigidbody>().useGravity = false;
-            other.gameObject.transform.SetParent(this.transform);
-            other.gameObject.transform.position = new Vector3(0f, 0f, 0f);
-            Debug.Log("Parented");
-            StartCoroutine(CheckFalse(other.gameObject));
+            // Debug.Log("true");
+            // // attachedObject = other.gameObject;
+            // if (other.tag == "Item") {
+            // //    other.gameObject.GetComponent<Rigidbody>().useGravity = false;
+            //     other.gameObject.transform.SetParent(this.transform);
+            //     // other.gameObject.transform.position = new Vector3(0f, 0f, 0f);
+            //     Debug.Log("Parented");
+            //     StartCoroutine(CheckFalse(other.gameObject)); 
+            // }
+            if (other.tag == "Item") {
+                hook.gameObject.AddComponent<FixedJoint>()
+                .connectedBody=other.gameObject.GetComponent<Rigidbody>();
+            }
         }
     }
 
+    private void Update() {
+        if (buttonPress == false) {
+            FixedJoint joint = hook.gameObject.GetComponent<FixedJoint>();
+            Destroy(joint);
+        }
+    }
     IEnumerator CheckFalse(GameObject attached)
     {
         if (buttonPress == true) 
