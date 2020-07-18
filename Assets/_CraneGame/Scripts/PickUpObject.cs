f26f23c53a449dc14200b4d63e4ce197436c8f3d
Amanda Vasconcelos <avasconcelos28@gmail.com>
2020-07-18 13:21:05 -0400

    SFXController
    Added SFXController and cue for postive beep, negative beep, object picked up, and object dropped

---
+++ Assets/_CraneGame/Scripts/PickUpObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpObject : MonoBehaviour
+{
+    public SFXController sfxController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "GroundTag")
+        {
+            sfxController.OnPutDown();
+            sfxController.NegativeBeep();
+        }
+        else if (collision.gameObject.tag == "GoalZone")
+        {
+            sfxController.OnPutDown();
+            sfxController.PositiveBeep();
+        }
+    }
+}
---
+++ Assets/_CraneGame/Scripts/SFXController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXController : MonoBehaviour
+{
+    public AudioSource ObjectPickUp;
+    public AudioSource PositiveBeepSound;
+    public AudioSource NegativeBeepSound;
+    public AudioSource ObjectDropped;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPickedUp()
+    {
+        ObjectPickUp.Play();
+    }
+
+    public void OnPutDown()
+    {
+        ObjectPickUp.Play();
+    }
+
+    public void PositiveBeep()
+    {
+        ObjectPickUp.Play();
+    }
+
+    public void NegativeBeep()
+    {
+        ObjectPickUp.Play();
+    }
+}
