d76b33ec9b460a72ca5b98a61f5ecd58d259c3ab
Amanda Vasconcelos <avasconcelos28@gmail.com>
2020-07-21 15:50:20 -0400

    Added engine ambient and fixed timing

--- Assets/_CraneGame/Scripts/SFXController.cs
+++ Assets/_CraneGame/Scripts/SFXController.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 
 public class SFXController : MonoBehaviour
 {
@@ -26,40 +26,75 @@ public class SFXController : MonoBehaviour
 
     public void OnPickedUp()
     {
-        ObjectPickUp.Play();
+        //ObjectPickUp.Play();
+        ObjectPickUp.PlayOneShot(ObjectPickUp.clip);
     }
 
     public void OnPutDown()
     {
-        ObjectDropped.Play();
+        //ObjectDropped.Play();
+        ObjectDropped.PlayOneShot(ObjectDropped.clip);
     }
 
     public void PositiveBeep()
     {
-        PositiveBeepSound.Play();
+        //PositiveBeepSound.Play();
+        PositiveBeepSound.PlayOneShot(PositiveBeepSound.clip);
     }
 
     public void NegativeBeep()
     {
-        NegativeBeepSound.Play();
+        //NegativeBeepSound.Play();
+        NegativeBeepSound.PlayOneShot(NegativeBeepSound.clip);
     }
 
     public void OnEngineStart()
     {
         EngineOn.Play();
-        StartCoroutine(WaitForLoop());
+        //StartCoroutine(WaitForLoop());
+        StartCoroutine(Crossfade(EngineOn, EngineRunning, 2f));
 
     }
 
     public void OnEngineStop()
     {
-        EngineRunning.Stop();
-        EngineOff.Play();
+        //EngineRunning.Stop();
+        //EngineOff.Play();
+
+        StartCoroutine(Crossfade(EngineRunning, EngineOff, 1f));
     }
 
-    IEnumerator WaitForLoop()
-    {
-        yield return new WaitForSeconds(EngineOn.clip.length);
-        EngineRunning.Play();
-    }
+    //IEnumerator WaitForLoop()
+    //{
+    //    yield return new WaitForSeconds(EngineOn.clip.length);
+    //    EngineRunning.Play();
+    //}
+    IEnumerator Crossfade(AudioSource fadeout, AudioSource fadein, float fadetime)
+        {
+            float startfadeoutVolume = fadeout.volume;
+            float startfadeinVolume = 0.2f;
+            float endFadeinVolume = fadein.volume;
+
+            fadein.volume = startfadeinVolume;
+            fadein.Play();
+
+        while (/*fadeout.volume > 0 &&*/ fadein.volume <= endFadeinVolume)
+            {
+                Debug.Log("while");
+
+                if (fadeout.volume > 0)
+                {
+                    Debug.Log("if");
+                    fadeout.volume -= startfadeoutVolume * Time.deltaTime / fadetime;
+                }
+
+                fadein.volume += startfadeinVolume * Time.deltaTime / fadetime;
+                yield return null;
+            }
+
+            fadeout.Stop();
+            fadeout.volume = startfadeoutVolume;
+
+        }
+
 }
