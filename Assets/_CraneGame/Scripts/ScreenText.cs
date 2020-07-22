de7b733affb70b17f04ac41c80b20d166e069284
Steve Krueger <stevekrueger52@gmail.com>
2020-07-22 14:49:01 -0400

    LCD Screens and Simulation Controls (not yet implemented) added to cockpit view

---
+++ Assets/_CraneGame/Scripts/ScreenText.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenText : MonoBehaviour
+{
+    [SerializeField]
+    private Text _text;
+    public string text
+    {
+        get { return _text.text; }
+        set { _text.text = value; }
+    }
+
+    public string startingText;
+    private void OnValidate() { text = startingText; }
+}
